e50b33bb5d13affa6cefb01d433c73c1bacebedf
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-01-17 16:45:57 -0500

    add receita model

--- receita/admin.py
+++ receita/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models.receita_model import Receita
+
+
+@admin.register(Receita)
+class Receita(admin.ModelAdmin):
+    list_display = ('id', 'descricao', 'valor', 'created_at', 'updated_at')
+    list_filter = ('descricao', 'valor')
+    search_fields = ('descricao', 'valor')
+    list_per_page = 10
---
+++ receita/models/receita_model.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Receita(models.Model):
+    """Classe que representa um modelo de receita de um usuário."""
+
+    descricao = models.TextField(_('Descrição'), max_length=200)
+    valor = models.DecimalField(_('Valor'), max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(_('Criado em'), auto_now_add=True)
+    updated_at = models.DateTimeField(_('Atualizado em'), auto_now=True)
+
+    class Meta:
+        verbose_name = _('Receita')
+        verbose_name_plural = _('Receitas')
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.descricao[:10]} - {self.valor}'
