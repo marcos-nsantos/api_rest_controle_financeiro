7bd5345f82ccda9f76b98b7f96387005037b69d0
Marcos Santos <marcos.diego.ns@outlook.com>
2022-01-22 14:28:13 -0500

    add filter backends

--- setup/settings.py
+++ setup/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'drf_spectacular',
+    'django_filters',
     'income.apps.IncomeConfig',
     'expense.apps.ExpenseConfig',
 ]
@@ -56,6 +57,7 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 50,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
 SPECTACULAR_SETTINGS = {
