6cf568c879ed04c4254eab1771eac46ca33e18c7
Marcos Santos <marcos.diego.ns@outlook.com>
2022-01-22 17:39:34 -0500

    add endpoint to get incomes by month

--- apps/income/views.py
+++ apps/income/views.py
@@ -1,7 +1,9 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
-from .models.income_model import Income
-from .serializer import IncomeSerializer
+from apps.income.models.income_model import Income
+from apps.income.serializer import IncomeSerializer
 
 
 class IncomeViewSet(viewsets.ModelViewSet):
@@ -9,3 +11,18 @@ class IncomeViewSet(viewsets.ModelViewSet):
     queryset = Income.objects.all()
     serializer_class = IncomeSerializer
     filterset_fields = ('description',)
+
+    @action(detail=False, methods=['get'], url_path=r'(?P<year>\d{4})/(?P<month>\d{1,2})')
+    def get_income_by_month(self, request, year, month):
+        """Extra route to get incomes by month."""
+        incomes = Income.objects.filter(receipt_date__year=year, receipt_date__month=month)
+        if not incomes:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        page = self.paginate_queryset(incomes)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True, context={'request': request})
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(incomes, many=True, context={'request': request})
+        return Response(serializer.data)
