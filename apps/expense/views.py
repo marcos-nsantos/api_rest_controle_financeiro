7eb4a1edb6a38c986a100976ea4ae236bc4a524a
Marcos Santos <marcos.diego.ns@outlook.com>
2022-01-23 11:24:37 -0500

    add enpoint to get expenses by month

--- apps/expense/views.py
+++ apps/expense/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from .models.expense_model import Expense
 from .serializer import ExpenseSerializer
@@ -9,3 +11,18 @@ class ExpenseViewSet(viewsets.ModelViewSet):
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
     filterset_fields = ('description',)
+
+    @action(detail=False, methods=['get'], url_path=r'(?P<year>\d{4})/(?P<month>\d{1,2})')
+    def get_expense_by_month(self, request, year, month):
+        """Get expense by month."""
+        expenses = Expense.objects.filter(due_date__year=year, due_date__month=month)
+        if not expenses:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        page = self.paginate_queryset(expenses)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True, context={'request': request})
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(expenses, many=True, context={'request': request})
+        return Response(serializer.data)
