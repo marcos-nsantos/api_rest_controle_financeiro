47100b836c2176fe17e59661ff718bab3eaa15d1
Marcos Santos <marcos.diego.ns@outlook.com>
2022-01-24 09:49:28 -0500

    remove type enum

--- apps/income/models/income_model.py
+++ apps/income/models/income_model.py
@@ -5,11 +5,6 @@ from django.utils.translation import gettext_lazy as _
 class Income(models.Model):
     """Class to represent an income model."""
 
-    class IncomeType(models.IntegerChoices):
-        FIXED = 1, _('Fixed')
-        VARIABLE = 2, _('Variable')
-
-    type = models.IntegerField(choices=IncomeType.choices, null=False)
     description = models.CharField(max_length=100)
     value = models.DecimalField(max_digits=7, decimal_places=2)
     receipt_date = models.DateField()
